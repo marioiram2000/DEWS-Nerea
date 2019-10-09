fdf218953a2236b48c890749662b8b73ad6eb3f0
marioiram2000 <48942036+marioiram2000@users.noreply.github.com>
2019-10-09 09:24:12 +0200

    adminclientes añadidio

---
+++ 07BBDD/adminclientes.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<?php
+    session_start();
+    include_once 'bbdd.inc.php';
+    $cn=conectarse();
+?>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+            
+        ?>
+    </body>
+</html>
