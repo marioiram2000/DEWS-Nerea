6b74409f4ce9f22e27b6b4e1bcd25f13fad59fd0
marioiram2000 <48942036+marioiram2000@users.noreply.github.com>
2019-10-29 14:06:28 +0100

    commit

---
+++ CI_Prestamos/application/controllers/c_prestamos.php
@@ -0,0 +1,16 @@
+<?php
+
+class c_prestamos extends CI_Controller{
+    function __construct() {
+        parent::__construct();
+        
+    }
+    
+    public function index()
+    {
+        
+        $this->load->view("v_cabecera");
+
+        $this->load->view("v_pie");
+    }
+}
---
+++ CI_Prestamos/application/views/v_cabecera.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<?php $hojadeestilos = base_url()."css/stylesheet.css"?>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <div id="header">
+            <h1>PRESTAMOS</h1>            
+        </div>
+   
